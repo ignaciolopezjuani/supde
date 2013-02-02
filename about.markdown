8a0cd5f6bb1338d3e50aa225c7077919f5cf4131
Ignacio Lopez Juani <ignacio@lopezjuani.com>
2013-02-02 20:11:21 -0200

    Add basic pages

--- about.markdown
+++ about.markdown
@@ -1,6 +1,7 @@
 ---
 layout: default
 body_class: about
+title: About
 ---
 
 About me
---
+++ media/js/functions.js
@@ -0,0 +1,19 @@
+jQuery.noConflict();
+    (function($){
+        window.onload=function(){
+            $("#tS2").thumbnailScroller({
+                scrollerType:"hoverPrecise",
+                scrollerOrientation:"horizontal",
+                scrollSpeed:2,
+                scrollEasing:"easeOutCirc",
+                scrollEasingAmount:600,
+                acceleration:4,
+                scrollSpeed:800,
+                noScrollCenterSpace:10,
+                autoScrolling:0,
+                autoScrollingSpeed:2000,
+                autoScrollingEasing:"easeInOutQuad",
+                autoScrollingDelay:500
+            });
+        }
+    })(jQuery);
